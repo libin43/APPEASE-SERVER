e5f53e235ebb661db20029193b1cd84d8a6be354
Libin Biji <libinbiji43@gmail.com>
2023-11-24 18:49:09 +0530

    origin update

--- src/config/config.js
+++ src/config/config.js
@@ -3,6 +3,8 @@ dotenv.config()
 
 export default {
     port: process.env.PORT,
+    devOriginPort: process.env.DEV_ORIGIN_PORT,
+    productionOriginPort: process.env.PRODUCTION_ORIGIN_PORT,
     mongo: {
         uri: process.env.MONGO_URL
     },
